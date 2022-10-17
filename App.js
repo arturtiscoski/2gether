9046a113cb6f6abd14756ce89750b0342fc707f2
arturtiscoski <artur.tiscoski@innovaconnect.com.br>
2022-10-17 20:33:24 -0300

    FEAT: Adiciona navegação básica

--- App.js
+++ App.js
@@ -1,17 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { Agenda } from 'react-native-calendars';
+// In App.js in a new project
 
-export default function App() {
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AgendaComponent from './src/Agenda/AgendaComponent';
+
+function HomeScreen({ navigation }) {
   return (
-    <View/>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button title='Details' onPress={() => navigation.navigate('Details')}/>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button title='Home' onPress={() => navigation.navigate('Home')}/>
+      <Button title='Agenda' onPress={() => navigation.navigate('Agenda')}/>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Agenda" component={AgendaComponent} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
